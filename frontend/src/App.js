7f262d82a5cb35ad54114b01675929a5aec42291
AhmadQadourah <ahmad.h.qadoura@gmail.com>
2022-11-06 17:46:18 +0200

    login- reg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Routes, Route, Link , useNavigate ,useParams } from "react-router-dom";
+import {useEffect , useState , createContext} from "react"
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return <div
+
+   className="App">
+<Routes>
+      <Route path = "/login" element={<Login/> }  />
+      <Route path = "/register" element={<Register/> }  />
+</Routes>
+
+   </div>;
 }
 
 export default App;
