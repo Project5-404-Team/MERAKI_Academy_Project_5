7b4d0f7a93931f2676d58aac5d4f91ed192c5cd3
AhmadQadourah <ahmad.h.qadoura@gmail.com>
2022-11-08 21:10:30 +0200

    tfjeer

--- frontend/src/components/Redux/store.js
+++ frontend/src/components/Redux/store.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 import usersAuth from "./reducers/usersAuth"
 import CompaniesAuth from "./reducers/CompaniesAuth"
 import fav from "./reducers/fav/fav"
-
+import users from "./reducers/Users/users"
 export default configureStore({
     reducer: {
         usersAuth: usersAuth,
         CompaniesAuth :CompaniesAuth,
-        fav:fav
+        fav:fav,
+        users:users
     }
 })
